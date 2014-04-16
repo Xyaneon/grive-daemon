31a89fdae66acb940dfe98d1712164ab0609a807
Christopher Kyle Horton <christhehorton@gmail.com>
2014-04-16 01:32:23 -0400

    Add working recursive directory watching.

--- grive-daemon.cpp
+++ grive-daemon.cpp
@@ -25,6 +25,8 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
    http://www.ibm.com/developerworks/linux/library/l-ubuntu-inotify/index.html
    And this (for recursive directory watching):
    https://gist.github.com/pkrnjevic/6016356
+   And this too, for more recursive directory watching:
+   http://stackoverflow.com/a/11097815
    */
 
 #include <stdio.h>
@@ -42,6 +44,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include <iostream>
 #include <string>
 #include <map>
+#include <dirent.h>
 
 using std::map;
 using std::string;
@@ -52,6 +55,13 @@ using std::endl;
 #define BUFF_SIZE ((sizeof(struct inotify_event)+FILENAME_MAX)*1024)
 
 #define WATCH_FLAGS (IN_ATTRIB | IN_CREATE | IN_DELETE | IN_DELETE_SELF | IN_MODIFY | IN_MOVE_SELF | IN_MOVED_FROM | IN_MOVED_TO)
+
+int initialRecursiveWd()
+{
+  static int wd = -1;
+  wd--;
+  return wd;
+}
 
 class Watch {
     struct wd_elem {
@@ -84,7 +94,7 @@ public:
     // an idea borrowed from Windows change journals.
     string get( int wd ) {
         const wd_elem &elem = watch[wd];
-        return elem.pd == -1 ? elem.name : this->get(elem.pd) + "/" + elem.name;
+        return elem.pd <= -1 ? elem.name : this->get(elem.pd) + "/" + elem.name;
     }
     // Given a parent wd and name (provided in IN_DELETE events), return the watch descriptor.
     // Main purpose is to help remove directories from watch list.
@@ -103,6 +113,30 @@ public:
         cout << "number of watches=" << watch.size() << " & reverse watches=" << rwatch.size() << endl;
     }
 };
+
+struct DirectoryReader
+{
+    static void parseDirectory(string readingDirectory, int fd, Watch& watch);
+};
+
+void DirectoryReader::parseDirectory(string readingDirectory, int fd, Watch& watch)
+{
+  if (DIR *dp = opendir(readingDirectory.c_str()))
+  {
+    //cout << string(level, ' ') << readingDirectory << endl;
+    while (struct dirent *ep = readdir(dp))
+      if (ep->d_type == DT_DIR && ep->d_name[0] != '.')
+      {
+        int wd;
+        wd = inotify_add_watch(fd, readingDirectory.c_str(), WATCH_FLAGS);
+        watch.insert(initialRecursiveWd(), readingDirectory.c_str(), wd);
+        parseDirectory(readingDirectory + "/" + ep->d_name, fd, watch);
+      }
+    closedir(dp);
+  }
+  else
+    syslog (LOG_WARNING, "Couldn't open the directory %s.", readingDirectory.c_str());
+}
 
 static void daemonize()
 {
@@ -147,13 +181,14 @@ static void daemonize()
     }
 }
 
-static bool get_event (int fd, const char * target)
+static bool get_event (int fd, const char * target, Watch& watch)
 {
-  Watch watch;
   ssize_t len, i = 0;
   char action[81+FILENAME_MAX] = {0};
   char buff[BUFF_SIZE] = {0};
+  string current_dir, new_dir;
   bool sync_required = false;
+  int wd;
 
   len = read (fd, buff, BUFF_SIZE);
 
@@ -168,10 +203,32 @@ static bool get_event (int fd, const char * target)
     
     if (pevent->mask & IN_ATTRIB) 
        strcat(action, "'s metadata changed");
-    if (pevent->mask & IN_CREATE) 
-       strcat(action, " was created in a watched directory");
-    if (pevent->mask & IN_DELETE) 
-       strcat(action, " was deleted in a watched directory");
+    if (pevent->mask & IN_CREATE)
+    {
+        current_dir = watch.get(pevent->wd);
+        if (pevent->mask & IN_ISDIR)
+        {
+          new_dir = current_dir + "/" + pevent->name;
+          wd = inotify_add_watch(fd, new_dir.c_str(), WATCH_FLAGS);
+          watch.insert(pevent->wd, pevent->name, wd);
+          //printf("New directory %s created.\n", new_dir.c_str());
+        } else {
+          //printf("New file %s/%s created.\n", current_dir.c_str(), pevent->name);
+        }
+        strcat(action, " was created in a watched directory");
+    }
+    if (pevent->mask & IN_DELETE)
+    {
+        if (pevent->mask & IN_ISDIR) {
+            new_dir = watch.erase(pevent->wd, pevent->name, &wd);
+            inotify_rm_watch(fd, wd);
+            //printf( "Directory %s deleted.\n", new_dir.c_str() );
+        } else {
+            current_dir = watch.get(pevent->wd);
+            //printf( "File %s/%s deleted.\n", current_dir.c_str(), pevent->name );
+        }
+        strcat(action, " was deleted in a watched directory");
+    }
     if (pevent->mask & IN_DELETE_SELF) 
        strcat(action, ", the watched file/directory, was itself deleted");
     if (pevent->mask & IN_MODIFY) 
@@ -205,10 +262,9 @@ void handle_error (int error)
 
 int main()
 {
+    Watch watch;
     char target[FILENAME_MAX];
-    int result;
-    int fd;
-    int wd;
+    int result, fd, wd;
     struct passwd *pw = getpwuid(getuid());
     char *gd_dir = strcat(pw->pw_dir, "/Google Drive");
     
@@ -240,10 +296,13 @@ int main()
       handle_error (errno);
       return EXIT_FAILURE;
     }
+    watch.insert(-1, gd_dir, wd);
+    
+    DirectoryReader::parseDirectory(string(gd_dir), fd, watch);
     
     while (1)
     {
-        if (get_event(fd, target))
+        if (get_event(fd, target, watch))
         {
           system ("notify-send grive-daemon \"Starting Google Drive sync...\"");
           system ("grive");
@@ -254,6 +313,7 @@ int main()
     }
     
     inotify_rm_watch(fd, wd);
+    watch.cleanup(fd);
     close(fd);
     syslog (LOG_NOTICE, "grive-daemon terminated.");
     closelog();
